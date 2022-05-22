f7d8a8da348ae40d0234f83012bb77c637743db5
Rohit joshi <rohitjoshi9july@gmail.com>
2022-05-22 13:56:32 +0530

    "initial commit"

---
+++ app.py
@@ -0,0 +1,44 @@
+from helper.imgToText import OCR
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+
+global ocr
+ocr = OCR()
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+def start(update, context):
+    context.bot.send_message(update.message.chat.id,"Welcome to image 2 text converter bot!")
+
+def extract(update, context):
+    global ocr
+    usr_id = update.message.chat.id
+    try:
+        file = context.bot.get_file(update.message.photo[-1].file_id)
+        filename = file.download()
+        text = ocr.extract(filename)
+        context.bot.send_message(usr_id, "✅ Your extracted text: ")
+        context.bot.send_message(usr_id, text)
+    except Exception as e:
+        context.bot.send_message(usr_id,"Oops! Facing some issues. Try again later!")
+
+def main():
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.photo, extract))
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(os.environ.get("PORT", 8443)),
+                          url_path=BOT_TOKEN,
+                          webhook_url="https://textextractorbot.herokuapp.com"+BOT_TOKEN)
+
+    updater.idle()
+
+if __name__=="__main__":
+    main()
+
+
+
+
--- helper/imgToText.py
+++ helper/imgToText.py
@@ -1,11 +1,10 @@
 from pytesseract import pytesseract
 
-
 class OCR:
     def __init__(self):
         self.path = r"C:\Program Files\Tesseract-OCR\tesseract.exe" #change this if installed in amother path
 
-    def extract(self,filename):
+    def extract(self, filename):
 
         try:
             pytesseract.tesseract_cmd = self.path
@@ -16,9 +15,4 @@ class OCR:
             print(e)
             return "Error"
 
-if __name__ == "__main__":
-    obj = OCR()
-    imagepath = input("Enter your image path: ")
-    text = obj.extract(imagepath)
-    print("Your extracted text: \n\n", text)
 
